e3b4440e9c8a3f83f3c6ac95e1b76eb262690ac5
tajush <tajusabd@gmail.com>
2023-12-19 23:57:45 +0600

    all

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from '/public/logo.png'
+import Image from 'next/image'
+
+const Navbar = () => {
+  return (
+    <div>
+        <Image src={logo} alt='logo'></Image>
+        nav
+      
+    </div>
+  )
+}
+
+export default Navbar
