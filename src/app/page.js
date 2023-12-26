e8b9e864e48ee0192cd4081df8c13e857a4fcf32
tajush <tajusabd@gmail.com>
2023-12-26 12:59:51 +0600

    summary

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import Hero from './components/Hero';
 import CoreTracks from './components/CoreTracks';
 import SpecializedTracks from './components/SpecializedTracks';
 import Outcome from './components/Outcome';
-import DropDownMenu from './components/DropDownMenu';
+
 
 
 export default function Home() {
@@ -13,7 +13,7 @@ export default function Home() {
   
   return (
    <div >
-    <DropDownMenu />
+    
    
     <Navbar />
     <Hero />
